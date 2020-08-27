37d1e6cc21debd3c077cb4159c0d158cbc3b5302
Divyanshu Kumar <53843818+divyanshugit@users.noreply.github.com>
2020-08-27 06:51:34 +0530

    Update Task0.py
    Fixed the error. Because call.csv and text.csv are in a folder

--- Task0.py
+++ Task0.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('datasets/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('datasets/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
